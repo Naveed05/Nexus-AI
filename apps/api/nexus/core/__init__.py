f1a11e0971aaed8919f212379794599e7cabb2e4
Mirza Naveed Baig <naveedmirza173@gmail.com>
2026-09-18 12:21:08 +0530

    chore: clean core exports and expose model providers

--- apps/api/nexus/core/__init__.py
+++ apps/api/nexus/core/__init__.py
@@ -1,7 +1,11 @@
-from nexus.core.agent_runtime import RuntimeContext, RuntimeResult, UnifiedAgentRuntime
+from nexus.core.agent_runtime import RuntimeResult, UnifiedAgentRuntime
 from nexus.core.execution_state import ExecutionState, ExecutionStateMachine, ExecutionTransition
+from nexus.core.models import ModelProvider, ModelRegistry, ModelResponse, ModelSpec
+from nexus.core.provider import ModelProviderRegistry, StaticModelProvider, provider_registry
 from nexus.core.retry_policy import RetryPolicy
 from nexus.core.runtime_context import RuntimeContext
+from nexus.core.tool_execution import ToolExecutionResult, ToolExecutor
+from nexus.core.tool_security import ToolSecurityPolicy
 
 __all__ = [
     "ExecutionState",
@@ -11,7 +15,14 @@ __all__ = [
     "RuntimeResult",
     "UnifiedAgentRuntime",
     "RetryPolicy",
-    "RuntimeContext",
+    "ModelProvider",
+    "ModelRegistry",
+    "ModelResponse",
+    "ModelSpec",
+    "ModelProviderRegistry",
+    "StaticModelProvider",
+    "provider_registry",
+    "ToolExecutionResult",
+    "ToolExecutor",
+    "ToolSecurityPolicy",
 ]
-from nexus.core.tool_execution import ToolExecutionResult, ToolExecutor
-from nexus.core.tool_security import ToolSecurityPolicy
